612043aa5516eb52810128216e1f2c9ca6789105
EslamKamel89 <elsamkamelforex@gmail.com>
2025-07-25 18:41:45 +0300

    Scaffold Category/Index.vue and add sidebar navigation

---
+++ resources/js/pages/Categories/Index.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import AppLayout from '@/layouts/AppLayout.vue';
+import { type BreadcrumbItem } from '@/types';
+import { Head } from '@inertiajs/vue3';
+
+const breadcrumbs: BreadcrumbItem[] = [
+    {
+        title: 'Categories',
+        href: '/categories',
+    },
+];
+</script>
+
+<template>
+    <Head title="Categories" />
+
+    <AppLayout :breadcrumbs="breadcrumbs">
+        <div class="flex flex-col flex-1 h-full gap-4 p-4 overflow-x-auto rounded-xl">Categories</div>
+    </AppLayout>
+</template>
