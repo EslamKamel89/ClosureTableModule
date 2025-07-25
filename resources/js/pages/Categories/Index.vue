2ea1ab93c085a3057255912263e3950652f1fb2c
EslamKamel89 <elsamkamelforex@gmail.com>
2025-07-25 18:52:13 +0300

    Define TypeScript interfaces for CategoryNode and Breadcrumb

--- resources/js/pages/Categories/Index.vue
+++ resources/js/pages/Categories/Index.vue
@@ -2,19 +2,42 @@
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
+import { ref } from 'vue';
 
-const breadcrumbs: BreadcrumbItem[] = [
+const breadcrumbsLinks: BreadcrumbItem[] = [
     {
         title: 'Categories',
         href: '/categories',
     },
 ];
+interface CategoryNode {
+    id: number;
+    name: string;
+    slug: string;
+    is_leaf: boolean;
+    product_count: number;
+    children: CategoryNode[];
+}
+interface BreadcrumbType {
+    id: number;
+    name: string;
+    slug: string;
+    is_leaf?: boolean;
+}
+defineProps<{
+    tree: CategoryNode;
+}>();
+const breadcrumbs = ref<BreadcrumbType[]>([]);
 </script>
 
 <template>
     <Head title="Categories" />
 
-    <AppLayout :breadcrumbs="breadcrumbs">
-        <div class="flex flex-col flex-1 h-full gap-4 p-4 overflow-x-auto rounded-xl">Categories</div>
+    <AppLayout :breadcrumbs="breadcrumbsLinks">
+        <div class="flex flex-col flex-1 h-full gap-4 p-4 overflow-x-auto rounded-xl">
+            <pre>
+                {{ tree }}
+            </pre>
+        </div>
     </AppLayout>
 </template>
